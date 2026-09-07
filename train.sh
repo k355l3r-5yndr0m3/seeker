913a451f7b25f9db5f3112740f3b932e889be8e8
k355l3r-5yndr0m3 <hoanghung17jan@gmail.com>
2026-09-07 23:13:40 +0700

    add shebang to script

--- train.sh
+++ train.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 python seeker.py --epochs 20 --seg_len 24 --model_lr 1e-3 --batch_size 256 --sigma 10 --n_layers 3 --expansion_factor 4 --dataset UBnormal
 python seeker.py --epochs 20 --seg_len 24 --model_lr 1e-3 --batch_size 256 --sigma 10 --n_layers 3 --expansion_factor 4 --dataset UBnormal
 
